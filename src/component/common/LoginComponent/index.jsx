9b1658ed37186c2904a458f9a932184544f66dae
2tracker <project.tracker111@gmail.com>
2023-07-05 23:32:43 +0530

    Register and login Responsive

--- src/component/common/LoginComponent/index.jsx
+++ src/component/common/LoginComponent/index.jsx
@@ -88,22 +88,22 @@ function Login() {
 
 
       <div className="bg-no-repeat bg-cover bg-center relative">
-        <div className="absolute bg-login-page inset-0 z-0"></div>
-          <div className="container mx-auto">
-          <div className="min-h-screen sm:flex sm:flex-row mx-0 justify-center gap-8 ">
-          <div className="flex-col flex  self-center p-10 sm:max-w-5xl xl:max-w-2xl  z-10">
-            <div className="self-start  lg:flex flex-col  text-black">
+        <div className="absolute bg-login-page inset-0 z-0 max-[479px]:!h-full"></div>
+        <div className="container mx-auto !px-20 max-[1200px]:!px-0 max-[1024px]:!px-8  max-[991px]:min-w-full max-[479px]:py-20 max-[479px]:px-4">
+          <div className="min-h-screen grid grid-cols-2 mx-0 justify-center gap-8 max-[991px]:gap-0 max-[576px]:grid-cols-1  max-[576px]:content-center	">
+          <div className="flex-col flex  self-center p-10 max-[1024px]:!px-0 sm:max-w-5xl xl:max-w-2xl  z-10 max-[479px]:!pt-0">
+            <div className="self-start  lg:flex flex-col  text-black max-[576px]:self-center max-[576px]:text-center">
               <img src="" className="mb-3" />
-              <h1 className="mb-3 font-bold text-5xl opacity-[0.6]">
+              <h1 className="mb-3 font-bold text-5xl opacity-[0.6] max-[991px]:text-[42px] max-[771px]:text-[36px] max-[479px]:text-[32px]">
                 Hi ? Welcome Back Track{" "}
               </h1>
-              <p className="pr-3">
+              <p className="pr-3 max-[576px]:pr-0">
                 To keep connected with us please login with your personal details
               </p>
             </div>
           </div>
-          <div className="flex justify-center self-center  z-10">
-            <div className="p-12 bg-login-box mx-auto ">
+          <div className="flex justify-center self-center  z-10 w-[80%] mx-auto max-[1024px]:w-full">
+            <div className="p-12 bg-login-box mx-auto max-[576px]:p-8 ">
               <div className="mb-4">
                 <h3 className="font-semibold text-2xl text-gray-800">
                   Sign In to Track
