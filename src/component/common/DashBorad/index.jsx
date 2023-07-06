40bb3b4a1ea71afa4affb35581f007a40ab9bd06
2tracker <project.tracker111@gmail.com>
2023-07-06 23:15:05 +0530

    create dashboard

--- src/component/common/DashBorad/index.jsx
+++ src/component/common/DashBorad/index.jsx
@@ -159,31 +159,32 @@ function DashBorad() {
               </div>
             </div>
             <div className="fixed inset-y-0 z-20 flex-shrink-0 w-64 mt-16 overflow-y-auto bg-white dark:bg-gray-800 md:hidden">
-              <div className="py-4 text-gray-500 dark:text-gray-400">
-                <a
-                  className="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200"
-                  href="#"
-                >
-                  Windmill
-               </a>
-                <ul className="mt-6">
+            <div className="py-4 text-gray-500 dark:text-gray-400">
+                <ul className="">
                   <li className="relative px-6 py-3">
+                  <span
+                      className="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                      aria-hidden="true"
+                    ></span>
                     <a
                       className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                      href="index.html"
+                      href="#"
                     >
+                      {/* <img src={HomeIcon} className="" /> */}
                       <svg
-                        className="w-5 h-5"
-                        aria-hidden="true"
-                        fill="none"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
-                      </svg>
+                  className="w-5 h-5"
+                  aria-hidden="true"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                  ></path>
+                </svg>
                       <span className="ml-4">Dashboard</span>
                    </a>
                   </li>
@@ -212,7 +213,7 @@ function DashBorad() {
                   <li className="relative px-6 py-3">
                     <a
                       className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                      href="cards.html"
+                      href="#"
                     >
                       <svg
                         className="w-5 h-5"
@@ -230,13 +231,10 @@ function DashBorad() {
                    </a>
                   </li>
                   <li className="relative px-6 py-3">
-                    <span
-                      className="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
-                      aria-hidden="true"
-                    ></span>
+                    
                     <a
                       className="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100"
-                      href="charts.html"
+                      href="#"
                     >
                       <svg
                         className="w-5 h-5"
@@ -257,7 +255,7 @@ function DashBorad() {
                   <li className="relative px-6 py-3">
                     <a
                       className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                      href="buttons.html"
+                      href="#"
                     >
                       <svg
                         className="w-5 h-5"
@@ -274,126 +272,18 @@ function DashBorad() {
                       <span className="ml-4">Buttons</span>
                    </a>
                   </li>
-                  <li className="relative px-6 py-3">
-                    <a
-                      className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                      href="modals.html"
-                    >
-                      <svg
-                        className="w-5 h-5"
-                        aria-hidden="true"
-                        fill="none"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"></path>
-                      </svg>
-                      <span className="ml-4">Modals</span>
-                   </a>
-                  </li>
-                  <li className="relative px-6 py-3">
-                    <a
-                      className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                      href="tables.html"
-                    >
-                      <svg
-                        className="w-5 h-5"
-                        aria-hidden="true"
-                        fill="none"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path d="M4 6h16M4 10h16M4 14h16M4 18h16"></path>
-                      </svg>
-                      <span className="ml-4">Tables</span>
-                   </a>
-                  </li>
-                  <li className="relative px-6 py-3">
-                    <button
-                      className="inline-flex items-center justify-between w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                      aria-haspopup="true"
-                    >
-                      <span className="inline-flex items-center">
-                        <svg
-                          className="w-5 h-5"
-                          aria-hidden="true"
-                          fill="none"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                        >
-                          <path d="M4 5a1 1 0 011-1h14a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zM4 13a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H5a1 1 0 01-1-1v-6zM16 13a1 1 0 011-1h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6z"></path>
-                        </svg>
-                        <span className="ml-4">Pages</span>
-                      </span>
-                      <svg
-                        className="w-4 h-4"
-                        aria-hidden="true"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                          clip-rule="evenodd"
-                        ></path>
-                      </svg>
-                    </button>
-                    <template x-if="isPagesMenuOpen">
-                      <ul
-                        className="p-2 mt-2 space-y-2 overflow-hidden text-sm font-medium text-gray-500 rounded-md shadow-inner bg-gray-50 dark:text-gray-400 dark:bg-gray-900"
-                        aria-label="submenu"
-                      >
-                        <li className="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
-                          <a className="w-full" href="pages/login.html">
-                            Login
-                         </a>
-                        </li>
-                        <li className="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
-                          <a
-                            className="w-full"
-                            href="pages/create-account.html"
-                          >
-                            Create account
-                         </a>
-                        </li>
-                        <li className="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
-                          <a
-                            className="w-full"
-                            href="pages/forgot-password.html"
-                          >
-                            Forgot password
-                         </a>
-                        </li>
-                        <li className="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
-                          <a className="w-full" href="pages/404.html">
-                            404
-                         </a>
-                        </li>
-                        <li className="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
-                          <a className="w-full" href="pages/blank.html">
-                            Blank
-                         </a>
-                        </li>
-                      </ul>
-                    </template>
-                  </li>
+                  
+                  
                 </ul>
                 <div className="px-6 my-6">
-                  <button className="flex items-center justify-between px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
+                    <a href="/signup">
+                  <button className="flex items-center justify-between w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
                     Create account
                     <span className="ml-2" aria-hidden="true">
                       +
                     </span>
                   </button>
+                  </a>
                 </div>
               </div>
             </div>
