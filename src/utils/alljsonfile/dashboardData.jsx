5dbbee9934d983a0f5893ec7e0751d9cb55e290c
2tracker <project.tracker111@gmail.com>
2023-07-09 00:14:47 +0530

    dahboard ui Fixed

--- src/utils/alljsonfile/dashboardData.jsx
+++ src/utils/alljsonfile/dashboardData.jsx
@@ -1,6 +1,26 @@
-const DashBoardSideBar = [
-    {
-        sidebartitle:"Dashboard",
-        sidebaricon:"/assets/homeIcon.svg"
-    }
-]
+import { BsFillImageFill , BsReverseLayoutTextSidebarReverse , BsPerson } from "react-icons/bs";
+import { AiOutlineFileText , AiOutlineBarChart , AiOutlineMail , AiOutlineSetting , AiOutlineLogout } from "react-icons/ai";
+
+export const DashboardMenus = [
+  { title: "Dashboard" },
+  { title: "Pages" , icon: <AiOutlineFileText />},
+  { title: "Media", spacing: true  , icon: <BsFillImageFill />},
+  {
+    title: "Projects",
+    icon: <BsReverseLayoutTextSidebarReverse />,
+    submenu: true,
+    submenuItems: [
+      { title: "Submenu 1" },
+      { title: "Submenu 2" },
+      { title: "Submenu 3" },
+    ],
+  },
+
+  { title: "Analytics" , icon: <AiOutlineBarChart /> },
+  { title: "Inbox" , icon: <AiOutlineMail /> },
+
+  { title: "Profile", spacing: true ,icon: <BsPerson />  },
+  { title: "Setting" , icon: <AiOutlineSetting />},
+
+  { title: "Logout" , icon: <AiOutlineLogout />},
+];
